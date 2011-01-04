926654296339d88f69cd481d1cf8eb9facdf9bf3
Klaus Kurzmann <mok@fluxnetz.de>
2011-01-04 18:08:17 +0100

    initial commit
    Signed-off-by: Klaus Kurzmann <mok@fluxnetz.de>

---
+++ src/shr-marshallers.h
@@ -0,0 +1,84 @@
+
+#ifndef ___shr_gdbus_cclosure_marshaller_MARSHAL_H__
+#define ___shr_gdbus_cclosure_marshaller_MARSHAL_H__
+
+#include	<glib-object.h>
+
+G_BEGIN_DECLS
+
+/* BOOLEAN:OBJECT,BOOLEAN (shr-marshallers.list:1) */
+extern void _shr_gdbus_cclosure_marshaller_BOOLEAN__OBJECT_BOOLEAN (GClosure     *closure,
+                                                                    GValue       *return_value,
+                                                                    guint         n_param_values,
+                                                                    const GValue *param_values,
+                                                                    gpointer      invocation_hint,
+                                                                    gpointer      marshal_data);
+
+/* BOOLEAN:OBJECT (shr-marshallers.list:2) */
+extern void _shr_gdbus_cclosure_marshaller_BOOLEAN__OBJECT (GClosure     *closure,
+                                                            GValue       *return_value,
+                                                            guint         n_param_values,
+                                                            const GValue *param_values,
+                                                            gpointer      invocation_hint,
+                                                            gpointer      marshal_data);
+
+/* BOOLEAN:OBJECT,INT (shr-marshallers.list:3) */
+extern void _shr_gdbus_cclosure_marshaller_BOOLEAN__OBJECT_INT (GClosure     *closure,
+                                                                GValue       *return_value,
+                                                                guint         n_param_values,
+                                                                const GValue *param_values,
+                                                                gpointer      invocation_hint,
+                                                                gpointer      marshal_data);
+
+/* BOOLEAN:OBJECT,STRING,STRING,STRING (shr-marshallers.list:4) */
+extern void _shr_gdbus_cclosure_marshaller_BOOLEAN__OBJECT_STRING_STRING_STRING (GClosure     *closure,
+                                                                                 GValue       *return_value,
+                                                                                 guint         n_param_values,
+                                                                                 const GValue *param_values,
+                                                                                 gpointer      invocation_hint,
+                                                                                 gpointer      marshal_data);
+
+/* BOOLEAN:OBJECT,STRING (shr-marshallers.list:5) */
+extern void _shr_gdbus_cclosure_marshaller_BOOLEAN__OBJECT_STRING (GClosure     *closure,
+                                                                   GValue       *return_value,
+                                                                   guint         n_param_values,
+                                                                   const GValue *param_values,
+                                                                   gpointer      invocation_hint,
+                                                                   gpointer      marshal_data);
+
+/* BOOLEAN:OBJECT,INT,INT,STRING (shr-marshallers.list:6) */
+extern void _shr_gdbus_cclosure_marshaller_BOOLEAN__OBJECT_INT_INT_STRING (GClosure     *closure,
+                                                                           GValue       *return_value,
+                                                                           guint         n_param_values,
+                                                                           const GValue *param_values,
+                                                                           gpointer      invocation_hint,
+                                                                           gpointer      marshal_data);
+
+/* BOOLEAN:OBJECT,VARIANT (shr-marshallers.list:7) */
+extern void _shr_gdbus_cclosure_marshaller_BOOLEAN__OBJECT_VARIANT (GClosure     *closure,
+                                                                    GValue       *return_value,
+                                                                    guint         n_param_values,
+                                                                    const GValue *param_values,
+                                                                    gpointer      invocation_hint,
+                                                                    gpointer      marshal_data);
+
+/* BOOLEAN:OBJECT,INT,STRING (shr-marshallers.list:8) */
+extern void _shr_gdbus_cclosure_marshaller_BOOLEAN__OBJECT_INT_STRING (GClosure     *closure,
+                                                                       GValue       *return_value,
+                                                                       guint         n_param_values,
+                                                                       const GValue *param_values,
+                                                                       gpointer      invocation_hint,
+                                                                       gpointer      marshal_data);
+
+/* BOOLEAN:OBJECT,STRING,STRING (shr-marshallers.list:9) */
+extern void _shr_gdbus_cclosure_marshaller_BOOLEAN__OBJECT_STRING_STRING (GClosure     *closure,
+                                                                          GValue       *return_value,
+                                                                          guint         n_param_values,
+                                                                          const GValue *param_values,
+                                                                          gpointer      invocation_hint,
+                                                                          gpointer      marshal_data);
+
+G_END_DECLS
+
+#endif /* ___shr_gdbus_cclosure_marshaller_MARSHAL_H__ */
+
